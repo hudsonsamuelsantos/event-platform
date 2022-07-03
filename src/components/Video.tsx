862d8f33945f2614ab26a4e9584f74fedc6e15db
Hudson Santos <hudsnss@gmail.com>
2022-07-03 14:45:50 -0300

    video component update

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,35 +1,92 @@
+import { DefaultUi, Player, Youtube } from "@vime/react";
 import { CaretRight, DiscordLogo, FileArrowDown, Lightning } from "phosphor-react";
+import { gql, useQuery } from "@apollo/client";
 
-export function Video() {
+import '@vime/core/themes/default.css'
+
+const GET_LESSON_BY_SLUG_QUERY = gql`
+    query GetLessonBySlug ($slug: String) {
+        lesson(where: {slug: $slug}) {
+            title
+            videoId
+            description
+            teacher {
+                bio
+                avatarURL
+                name
+        }
+      }
+    }
+`
+
+interface GetLessonBySlugResponse {
+    lesson: {
+        title: string;
+        videoId: string;
+        description: string;
+        teacher: {
+            bio: string;
+            avatarURL: string;
+            name: string;
+        }
+    }
+}
+
+interface VideoProps {
+    lessonSlug: string;
+}
+
+export function Video(props: VideoProps) {
+    const { data } = useQuery<GetLessonBySlugResponse>(GET_LESSON_BY_SLUG_QUERY, {
+        variables: {
+            slug: props.lessonSlug,
+        }
+    })
+
+    if (!data) {
+        return (
+            <div className="flex-1">
+                <p>
+                    Carregando...
+                </p>
+            </div>
+        )
+    }
+
     return (
         <div className="flex-1">
             <div className="bg-black flex justify-center">
-                <div className="h-full w-full max-w-[1100px] max-h-[60vh] aspect-video"></div>
+                <div className="h-full w-full max-w-[1100px] max-h-[60vh] aspect-video">
+                    <Player>
+                        <Youtube videoId={data.lesson.videoId} />
+                        <DefaultUi />
+                    </Player>
+                </div>
             </div>
 
             <div className="p-8 max-w-[1100px] mx-auto">
                 <div className="flex items-start gap-16">
                     <div className="flex-1">
                         <h1 className="text-2xl font-bold">
-                            Aula 01
+                            {data.lesson.title}
                         </h1>
                         <p className="mt-4 text-gray-200 leading-relaxed">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur ut obcaecati libero, aut numquam voluptas dicta, tempora provident beatae, repellendus corrupti officiis consectetur autem ipsum delectus quis omnis. Inventore, doloremque.
+                            {data.lesson.description}
                         </p>
 
                         <div className="flex items-center gap-4 mt-6">
                             <img
                                 className="h-16 w-16 rounded-full border-2 border-blue-500"
-                                src="https://github.com/hudsonsamuelsantos.png"
+                                src={data.lesson.teacher.avatarURL}
                                 alt=""
                             />
 
                             <div className="leading-relaxed">
                                 <strong className="font-bold text-2xl block">
-                                    Hudson Santos
+                                    {data.lesson.teacher.name}
                                 </strong>
                                 <span className="text-gray-200 text-sm block">
-                                    Front-End Developer
+                                    {data.lesson.teacher.bio}
                                 </span>
                             </div>
 
